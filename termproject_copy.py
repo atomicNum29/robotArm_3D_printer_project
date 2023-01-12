8d6528436522e24f42018b61f33402d11f275622
atomicNum29 <atomicw63.546@gmail.com>
2023-01-13 01:18:50 +0900

    1 tap only UI

--- termproject_copy.py
+++ termproject_copy.py
@@ -1,7 +1,58 @@
 
 import tkinter    # GUI tkinter 모듈.
+import tkinter.ttk    # GUI 추가 위젯 모듈.
+
+# GUI내에서 다른 탭으로 이동시 호출되는 함수.
+def tabchange(event):
+    print(notebook.select())
+    if notebook.select() == '.!frame':
+        tab1refresh()
+
+# 1번 탭으로 이동했을 때 1번 탭 리프레쉬.
+def tab1refresh():
+    global frame1L, frame1R, frameR_t
+
+    frame1L=tkinter.Frame(frame1, relief="groove", bd=2)
+    frame1L.place(x=0,y=0, width=800, height=552)
+
+    buttonL1 = tkinter.Button(frame1L, text="Plot", relief="solid", command=None)
+    buttonL1.place(x=15, y=499, width=90, height=41)
+    buttonL2 = tkinter.Button(frame1L, text="Edit Gcode", relief="solid", command=None)
+    buttonL2.place(x=110, y=499, width=90, height=41)
+    buttonL3 = tkinter.Button(frame1L, text="Save as.. ", relief="solid", command=None)
+    buttonL3.place(x=205, y=499, width=90, height=41)
+
+    frame1R = tkinter.Frame(frame1, relief="groove", bd=2)
+    frame1R.place(x=800,y=0, width=258, height=552)
+    frameR_t = tkinter.Frame(frame1R, relief='groove', bd=2)
+    frameR_t.place(x=12, y=270, width=230, height=270)
+    
+    buttonR1 = tkinter.Button(frame1R, text="Import Gcode", relief="solid", command=None)
+    buttonR1.place(x=12, y=30, width=230, height=30)
+    buttonR2 = tkinter.Button(frame1R, text="Check Gcode", relief="solid", command=None)
+    buttonR2.place(x=12, y=75, width=230, height=30)
+    buttonR3 = tkinter.Button(frame1R, text="See Filtered gcode", relief="solid", command=None)
+    buttonR3.place(x=12, y=120, width=230, height=30)
+    buttonR3 = tkinter.Button(frame1R, text="Export", relief="solid", command=None)
+    buttonR3.place(x=12, y=165, width=230, height=30)
+    clearbtn = tkinter.Button(frame1R, text="clear", relief="solid", command=None)
+    clearbtn.place(x=195, y=244, width=48, height=25)
 
 # GUI 생성 및 기본 창 설정.
 window = tkinter.Tk()
+window.title('Gcode Interpreter v1')
+window.geometry('1112x616+150+85')
+window.resizable(False, False)
+notebook=tkinter.ttk.Notebook(window, width=1112, height=616)
+notebook.pack()
+frame1=tkinter.Frame(window)
+notebook.add(frame1, text="  pre-process  ")
+frame2=tkinter.Frame(window)
+notebook.add(frame2, text=" after-process ")
+frame3 = tkinter.Frame(window)
+notebook.add(frame3, text="     exit     ")
+
+# 노트북 바인딩 및 GUI 메인루프 실행.
+notebook.bind('<<NotebookTabChanged>>',tabchange)
 
 window.mainloop()
