d776eccfe474cf7b02f650f8e2c893b6bb1c1b47
atomicNum29 <atomicw63.546@gmail.com>
2023-01-13 01:26:10 +0900

    2 tab only UI

--- termproject_copy.py
+++ termproject_copy.py
@@ -4,9 +4,12 @@ import tkinter.ttk    # GUI 추가 위젯 모듈.
 
 # GUI내에서 다른 탭으로 이동시 호출되는 함수.
 def tabchange(event):
-    print(notebook.select())
-    if notebook.select() == '.!frame':
+    tab_id = notebook.select()
+    print(tab_id)
+    if tab_id == '.!frame':
         tab1refresh()
+    elif tab_id == '.!frame2':
+        tab2refresh()
 
 # 1번 탭으로 이동했을 때 1번 탭 리프레쉬.
 def tab1refresh():
@@ -36,6 +39,36 @@ def tab1refresh():
     buttonR3 = tkinter.Button(frame1R, text="Export", relief="solid", command=None)
     buttonR3.place(x=12, y=165, width=230, height=30)
     clearbtn = tkinter.Button(frame1R, text="clear", relief="solid", command=None)
+    clearbtn.place(x=195, y=244, width=48, height=25)
+
+# 2번 탭으로 이동했을 때 2번 탭 리프레쉬.
+def tab2refresh():
+    global frame2L, frame2R, frameR_t
+
+    frame2L = tkinter.Frame(frame2, relief="groove", bd=2, width=500)
+    frame2L.place(x=0,y=0, width=800, height=552)
+
+    buttonL1 = tkinter.Button(frame2L, text="Plot FK", relief="solid", command=None)
+    buttonL1.place(x=15, y=499, width=90, height=41)
+    buttonL2 = tkinter.Button(frame2L, text="Edit Angles", relief="solid", command=None)
+    buttonL2.place(x=110, y=499, width=90, height=41)
+    buttonL3 = tkinter.Button(frame2L, text="Edit Done", relief="solid", command=None)
+    buttonL3.place(x=205, y=499, width=90, height=41)
+
+    frame2R = tkinter.Frame(frame2, relief='groove', bd=2)
+    frame2R.place(x=800,y=0, width=258, height=552)
+    frameR_t = tkinter.Frame(frame2R, relief='groove', bd=2)
+    frameR_t.place(x=12, y=270, width=230, height=270)
+
+    buttonR1 = tkinter.Button(frame2R, text="Import xlsx", relief="solid", command=None)
+    buttonR1.place(x=12, y=30, width=230, height=30)
+    buttonR2 = tkinter.Button(frame2R, text="Check Angles", relief="solid", command=None)
+    buttonR2.place(x=12, y=75, width=230, height=30)
+    buttonR3 = tkinter.Button(frame2R, text="See Angles", relief="solid", command=None)
+    buttonR3.place(x=12, y=120, width=230, height=30)
+    buttonR3 = tkinter.Button(frame2R, text="Export", relief="solid", command=None)
+    buttonR3.place(x=12, y=165, width=230, height=30)
+    clearbtn = tkinter.Button(frame2R, text="clear", relief="solid", command=None)
     clearbtn.place(x=195, y=244, width=48, height=25)
 
 # GUI 생성 및 기본 창 설정.
